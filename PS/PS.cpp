a122ba37d4d1e866c56c2b56bff95140005b923c
HaejinYang <haejin.yang91@gmail.com>
2019-08-16 14:46:34 +0900

    PS 1707 이분 그래프 : https://www.acmicpc.net/problem/1707 다시풀자.

--- PS/PS.cpp
+++ PS/PS.cpp
@@ -1,67 +1,155 @@
 ﻿#include <iostream>
 #include <vector>
 #include <utility>
-
-// 3초
+#include <queue>
+// 2초
 // 256MB
-constexpr int VERTEX_COUNT_MAX = 1000;
-constexpr int EDGE_COUNT_MAX = (VERTEX_COUNT_MAX * (VERTEX_COUNT_MAX - 1)) / 2;
-std::vector<int> gGraph[VERTEX_COUNT_MAX + 1];
-bool gbVisited[VERTEX_COUNT_MAX + 1];
-int gConnectedComponentCount = 0;
 
-void DFS(int start)
+enum class eColor
 {
-	gbVisited[start] = true;
+	NotVisited = 0,
+	Red = 1,
+	Blue = 2
+};
 
-	size_t edgeSize = gGraph[start].size();
+constexpr short VERTEX_MAX = 20000;
+constexpr int EDGE_MAX = 200000;
+std::vector<short> gGraph[VERTEX_MAX + 1];
+eColor gColor[VERTEX_MAX + 1];
 
-	for (int index = 0; index < edgeSize; index++)
+void DFS(short start, eColor color)
+{
+	gColor[start] = color;
+	size_t size = gGraph[start].size();
+
+	for (short index = 0; index < size; index++)
 	{
-		int vertex = gGraph[start][index];
+		short vertex = gGraph[start][index];
 
-		if (gbVisited[vertex] == false)
+		if (gColor[vertex] == eColor::NotVisited)
 		{
-			DFS(vertex);
+			if (color == eColor::Blue)
+			{
+				DFS(vertex, eColor::Red);
+			}
+			else
+			{
+				DFS(vertex, eColor::Blue);
+			}
+		}
+	}
+}
+
+void BFS(int start)
+{
+	std::queue<short> vertexQueue;
+
+	if (gColor[start] != eColor::NotVisited)
+	{
+		return;
+	}
+
+	vertexQueue.push(start);
+	gColor[start] = eColor::Red;
+
+	while (vertexQueue.empty() == false)
+	{
+		short vertex = vertexQueue.front();
+		vertexQueue.pop();
+
+		size_t size = gGraph[vertex].size();
+
+		for (short index = 0; index < size; index++)
+		{
+			short node = gGraph[vertex][index];
+
+			if (gColor[node] == eColor::NotVisited)
+			{
+				if (gColor[vertex] == eColor::Red)
+				{
+					gColor[node] = eColor::Blue;
+				}
+				else
+				{
+					gColor[node] = eColor::Red;
+				}
+
+				vertexQueue.push(node);
+			}
 		}
 	}
 }
 
 int main(void)
 {
-	std::ios_base::sync_with_stdio(false);
+	std::cin.sync_with_stdio(false);
 
-	int vertexCount;
-	int edgeCount;
-	std::cin >> vertexCount >> edgeCount;
-
-	int inputCount = edgeCount;
-
-	while (inputCount--)
+	short testCaseCount;
+	std::cin >> testCaseCount;	
+	
+	while (testCaseCount--)
 	{
-		int vertex1;
-		int vertex2;
-		std::cin >> vertex1 >> vertex2;
-
-		gGraph[vertex1].push_back(vertex2);
-		gGraph[vertex2].push_back(vertex1);
-	}
-
-	for (int index = 1; index <= VERTEX_COUNT_MAX; index++)
-	{
-		gbVisited[index] = false;
-	}
-
-	for (int index = 1; index <= vertexCount; index++)
-	{
-		if (gbVisited[index] == false)
+		for (short index = 1; index <= VERTEX_MAX; index++)
 		{
-			DFS(index);
-			gConnectedComponentCount++;
+			gColor[index] = eColor::NotVisited;
+			gGraph[index].clear();
+		}
+
+		short vertexCount;
+		int edgeCount;
+		std::cin >> vertexCount >> edgeCount;
+
+		for (int count = 0; count < edgeCount; count++)
+		{
+			short vertex1;
+			short vertex2;
+			std::cin >> vertex1 >> vertex2;
+
+			gGraph[vertex1].push_back(vertex2);
+			gGraph[vertex2].push_back(vertex1);
+		}
+		// --그래프 완성 끝--
+
+		for (int count = 1; count <= vertexCount; count++)
+		{
+			if (gColor[count] == eColor::NotVisited)
+			{
+				DFS(count, eColor::Red);
+			}
+		}
+
+		//DFS(1, eColor::Red);
+		// -- 색칠 끝 -- 		
+
+		bool bColorEqual = false;
+
+		for (int index = 1; index <= vertexCount; index++)
+		{
+			size_t size = gGraph[index].size();
+			eColor color = gColor[index];
+
+			for (short count = 0; count < size; count++)
+			{
+				short vertex = gGraph[index][count];
+
+				if (color == gColor[vertex])
+				{
+					bColorEqual = true;
+
+					break;
+				}
+			}
+		}
+
+		if (bColorEqual)
+		{
+			std::cout << "NO\n";
+		}
+		else
+		{
+			std::cout << "YES\n";
 		}
 	}
-
-	std::cout << gConnectedComponentCount << "\n";
 
 	return 0;
 }
---
+++ PS/PS1707.cpp
@@ -0,0 +1,172 @@
+﻿#include <iostream>
+#include <vector>
+#include <utility>
+#include <queue>
+// 2초
+// 256MB
+
+/**
+	@brief PS1707 이분 그래프 : https://www.acmicpc.net/problem/1707
+	다시풀어 왜?
+	1) 잔 실수가 너무 많았다. <= 나 <  혹은 vertexCount vs edgeCount 집중력 최악
+	2) 이분 그래프에 대한 이해 부족
+	start를 1만 하면 안되고 다른 것도 해야함왜?
+	A  -- E
+
+	B  -- F
+
+	C  -- G
+
+	이것도 이분그래프거등
+*/
+namespace ps1707
+{
+	enum class eColor
+	{
+		NotVisited = 0,
+		Red = 1,
+		Blue = 2
+	};
+
+	constexpr short VERTEX_MAX = 20000;
+	constexpr int EDGE_MAX = 200000;
+	std::vector<short> gGraph[VERTEX_MAX + 1];
+	eColor gColor[VERTEX_MAX + 1];
+
+	void DFS(short start, eColor color)
+	{
+		gColor[start] = color;
+		size_t size = gGraph[start].size();
+
+		for (short index = 0; index < size; index++)
+		{
+			short vertex = gGraph[start][index];
+
+			if (gColor[vertex] == eColor::NotVisited)
+			{
+				if (color == eColor::Blue)
+				{
+					DFS(vertex, eColor::Red);
+				}
+				else
+				{
+					DFS(vertex, eColor::Blue);
+				}
+			}
+		}
+	}
+
+	void BFS(int start)
+	{
+		std::queue<short> vertexQueue;
+
+		if (gColor[start] != eColor::NotVisited)
+		{
+			return;
+		}
+
+		vertexQueue.push(start);
+		gColor[start] = eColor::Red;
+
+		while (vertexQueue.empty() == false)
+		{
+			short vertex = vertexQueue.front();
+			vertexQueue.pop();
+
+			size_t size = gGraph[vertex].size();
+
+			for (short index = 0; index < size; index++)
+			{
+				short node = gGraph[vertex][index];
+
+				if (gColor[node] == eColor::NotVisited)
+				{
+					if (gColor[vertex] == eColor::Red)
+					{
+						gColor[node] = eColor::Blue;
+					}
+					else
+					{
+						gColor[node] = eColor::Red;
+					}
+
+					vertexQueue.push(node);
+				}
+			}
+		}
+	}
+
+	int main(void)
+	{
+		std::cin.sync_with_stdio(false);
+
+		short testCaseCount;
+		std::cin >> testCaseCount;
+
+		while (testCaseCount--)
+		{
+			for (short index = 1; index <= VERTEX_MAX; index++)
+			{
+				gColor[index] = eColor::NotVisited;
+				gGraph[index].clear();
+			}
+
+			short vertexCount;
+			int edgeCount;
+			std::cin >> vertexCount >> edgeCount;
+
+			for (int count = 0; count < edgeCount; count++)
+			{
+				short vertex1;
+				short vertex2;
+				std::cin >> vertex1 >> vertex2;
+
+				gGraph[vertex1].push_back(vertex2);
+				gGraph[vertex2].push_back(vertex1);
+			}
+			// --그래프 완성 끝--
+
+			for (int count = 1; count <= vertexCount; count++)
+			{
+				if (gColor[count] == eColor::NotVisited)
+				{
+					DFS(count, eColor::Red);
+				}
+			}
+
+			//DFS(1, eColor::Red);
+			// -- 색칠 끝 -- 		
+
+			bool bColorEqual = false;
+
+			for (int index = 1; index <= vertexCount; index++)
+			{
+				size_t size = gGraph[index].size();
+				eColor color = gColor[index];
+
+				for (short count = 0; count < size; count++)
+				{
+					short vertex = gGraph[index][count];
+
+					if (color == gColor[vertex])
+					{
+						bColorEqual = true;
+
+						break;
+					}
+				}
+			}
+
+			if (bColorEqual)
+			{
+				std::cout << "NO\n";
+			}
+			else
+			{
+				std::cout << "YES\n";
+			}
+		}
+
+		return 0;
+	}
+}
