a4e58e5457fc787bd5d21b10f25e55f169916815
HaejinYang <haejin.yang91@gmail.com>
2019-08-16 10:28:00 +0900

    PS11725 트리의 부모 찾기 : https://www.acmicpc.net/problem/11725

--- PS/PS.cpp
+++ PS/PS.cpp
@@ -1,126 +1,52 @@
 ﻿#include <iostream>
-#include <cstdint>
 #include <vector>
-#include <utility>
-#include <assert.h>
-#include <queue>
 
-constexpr int MAX_NUM = 10000;
-int gMaxNodeIndex = 0;
-int gMaxNodeDistance = 0;
+constexpr int MAX_NUM = 100001;
 
-std::vector<std::pair<int, int>> tree[MAX_NUM + 1];
-bool bVisitedNode[MAX_NUM + 1];
-int distance[MAX_NUM + 1];
+std::vector<int> gTree[MAX_NUM];
+int gParent[MAX_NUM];
+bool bVisited[MAX_NUM];
 
-void DFS(std::vector<std::pair<int, int>>* const tree, int index, bool* const bVisitedNode, int* const distance)
+void DFS(int start)
 {
-	bVisitedNode[index] = true;
-	size_t size = tree[index].size();
+	bVisited[start] = true;
+	auto size = gTree[start].size();
 
-	for (size_t count = 0; count < size; count++)
+	for (int index = 0; index < size; index++)
 	{
-		auto node = tree[index][count].first;
+		auto node = gTree[start][index];
 
-		if (bVisitedNode[node] == false)
+		if (bVisited[node] == false)
 		{
-			distance[node] = tree[index][count].second + distance[index];
-
-			if (distance[node] > gMaxNodeDistance)
-			{
-				gMaxNodeDistance = distance[node];
-				gMaxNodeIndex = node;
-			}
-
-			DFS(tree, node, bVisitedNode, distance);
+			gParent[node] = start;
+			DFS(node);
 		}
-	}
-}
-
-void BFS(std::vector<std::pair<int, int>>* const tree, int index, bool* const bVisitedNode, int* const distance)
-{
-	std::queue<int> q;
-	bVisitedNode[index] = true;
-	q.push(index);
-
-	while (q.empty() == false)
-	{
-		int x = q.front();
-		q.pop();
-
-		for (int i = 0; i < tree[x].size(); i++)
-		{
-			std::pair<int, int>& edge = tree[x][i];
-
-			if (bVisitedNode[edge.first] == false)
-			{
-				distance[edge.first] = distance[x] + edge.second;
-				q.push(edge.first);
-				bVisitedNode[edge.first] = true;
-
-				if (distance[edge.first] > gMaxNodeDistance)
-				{
-					gMaxNodeDistance = distance[edge.first];
-					gMaxNodeIndex = edge.first;
-				}
-			}
-		}
-	}
-}
-
-void ClearVisitedNode(bool* bVisitedNode, int num)
-{
-	assert(num > 0 && num < (MAX_NUM + 1));
-	assert(bVisitedNode != nullptr);
-
-	for (int index = 1; index <= num; index++)
-	{
-		bVisitedNode[index] = false;
-	}
-}
-
-void ClearDistance(int* distance, int num)
-{
-	assert(num > 0 && num < (MAX_NUM + 1));
-	assert(distance != nullptr);
-
-	for (int index = 1; index <= num; index++)
-	{
-		distance[index] = 0;
 	}
 }
 
 int main(void)
 {
 	std::ios_base::sync_with_stdio(false);
-
+	
 	int num;
 	std::cin >> num;
 
-	ClearVisitedNode(bVisitedNode, MAX_NUM);
-	ClearDistance(distance, MAX_NUM);
-
 	int count = num - 1;
-	
+
 	while (count--)
 	{
-		int parent; // 루트는 1
-		int child; 
-		int weight; // 가중치 100보다 작음
+		int vertex1;
+		int vertex2;
 
-		std::cin >> parent >> child >> weight;
-		tree[parent].push_back(std::pair<int, int>(child, weight));
-		tree[child].push_back(std::pair<int, int>(parent, weight));
+		std::cin >> vertex1 >> vertex2;
+		gTree[vertex1].push_back(vertex2);
+		gTree[vertex2].push_back(vertex1);
 	}
 
-	BFS(tree, 1, bVisitedNode, distance);
+	DFS(1);
 
-	ClearVisitedNode(bVisitedNode, MAX_NUM);
-	ClearDistance(distance, MAX_NUM);
-
-	BFS(tree, gMaxNodeIndex, bVisitedNode, distance);
-
-	std::cout << gMaxNodeDistance << std::endl;
-
-	return 0;
+	for (int index = 2; index <= num; index++)
+	{
+		std::cout << gParent[index] << "\n";
+	}
 }
---
+++ PS/PS11725.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+
+/**
+	@brief PS11725 트리의 부모 찾기 : https://www.acmicpc.net/problem/11725
+	@details
+	한 번에 못풀었음.
+	왜?
+	시간초과가 났기 때문에.
+	왜?
+	std::endl를 사용하여
+	사실 여태까지 별거 아니라고 생각했는데, 매번 flush를 해주는 일이 생각보다 비용이 엄청나군
+	앞으론 \n로 통일
+*/
+namespace PS11725
+{
+	constexpr int MAX_NUM = 100001;
+
+	std::vector<int> gTree[MAX_NUM];
+	int gParent[MAX_NUM];
+	bool bVisited[MAX_NUM];
+
+	void DFS(int start)
+	{
+		bVisited[start] = true;
+		auto size = gTree[start].size();
+
+		for (int index = 0; index < size; index++)
+		{
+			auto node = gTree[start][index];
+
+			if (bVisited[node] == false)
+			{
+				gParent[node] = start;
+				DFS(node);
+			}
+		}
+	}
+
+	int main(void)
+	{
+		std::ios_base::sync_with_stdio(false);
+
+		int num;
+		std::cin >> num;
+
+		int count = num - 1;
+
+		while (count--)
+		{
+			int vertex1;
+			int vertex2;
+
+			std::cin >> vertex1 >> vertex2;
+			gTree[vertex1].push_back(vertex2);
+			gTree[vertex2].push_back(vertex1);
+		}
+
+		DFS(1);
+
+		for (int index = 2; index <= num; index++)
+		{
+			std::cout << gParent[index] << "\n";
+		}
+	}
+}
